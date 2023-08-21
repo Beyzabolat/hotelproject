f1d40c8689d86161f780753977353f580a856159
BeyzaBolat <beyzabolat_@outlook.com>
2023-08-21 16:05:01 +0300

    Initial commit

---
+++ ApiConsume/HotelProject.WebApi/Mapping/autoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class autoMapperConfig : Profile
+    {
+        public autoMapperConfig()
+        { 
+        CreateMap <RoomAddDto, Room>();
+        CreateMap<Room, RoomAddDto>();
+            CreateMap<UpdateRoomDto, Room>().ReverseMap();
+
+
+        }
+         
+    }
+}
---
+++ FrontEnd/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "İsim alanı zorunludur")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Soyisim alanı zorunludur")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Kullanıcı adı alanı zorunludur")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Mail alanı zorunludur")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Şifre alanı zorunludur")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre tekrar alanı zorunludur")]
+        [Compare("Password", ErrorMessage ="Şifreler uyuşmuyor!")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
